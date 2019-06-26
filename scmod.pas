996bf4fd9a8f7b783efac378d390f80db7e4bbba
Perry van den Hondel <perry.vandenhondel@gmail.com>
2019-06-26 16:02:58 +0200

    Added procedure WriteRecordToConfig

--- scmod.pas
+++ scmod.pas
@@ -1,11 +1,10 @@
-program ModifyServerClass;
+program ServerClassModifier;
 
 //
-// Modify Splunk serverclass.conf
+// Modify the Splunk serverclass.conf
 //
 // Version 0.3
 //
-
 
 
 {$mode objfpc}
@@ -34,26 +33,58 @@ var
     hostArray: THostArray;
 
 
-function InHostArray(host: AnsiString): Boolean;
+function GetPathServerClass(): AnsiString;
+//
+// Get the path to the serverclass.conf file.
+// Location is stored in scmod.conf
+//
+var
+    path: AnsiString;
+    r: AnsiString;
+    tf: TextFile;
+begin
+    r := '';
+
+    path := ParamStr(0) +'.conf';
+    
+    AssignFile(tf, path);
+    Reset(tf);
+
+    ReadLn(tf, r);
+
+    Close(tf);
+
+    GetPathServerClass := r;
+end; // of function ReadLocationServerClass
+
+
+
+function HostRecordFind(host: AnsiString): Integer;
 //
 // Search for host in hostArray
 // Returns:
-//   true > found the name in the array
-//   false > Did not found the host in the array
-//
+//   -1: Not found
+//   >=0: Position of found host
 var
-    r: Boolean;
-    i: integer;
+    r: Integer;
+    i: Integer;
 begin
-    r := false;
+    r := -1;
 
     for i := 0 to High(hostArray) do
     begin
+        
         if host = hostArray[i].host then
-            r := true;
+        begin
+            // WriteLn('HostRecordFind() FOUND ', i, ': ', host, ' = ', hostArray[i].host);
+            r := i;
+            // We know the number; Break this loop.
+            Break;
+        end; // of if
     end; // of for
-    InHostArray := r;
-end; // of function InHostArray();
+
+    HostRecordFind := r;
+end; // of function HostRecordFind();
 
 
 procedure HostRecordAdd(host: AnsiString);
@@ -67,7 +98,7 @@ var
 begin
     writeln('HostRecordAdd(): ', host);
 
-    if InHostArray(host) = false then 
+    if HostRecordFind(host) = -1 then 
     begin
         // The host does not exist in the array; add it.
 
@@ -80,24 +111,51 @@ begin
         // Assign the host to the hostArray.host field.
         hostArray[size].host := host; 
     end;
-end; // of procedure AddRecord
+end; // of procedure HostRecordAdd
+
+
+procedure HostRecordDel(host: AnsiString);
+//
+// Delete a item from a dynamic array.
+// Source: https://www.tweaking4all.com/forums/topic/pascal-how-to-remove-an-array-element/
+//
+var
+    pos: Integer;
+    ALength: Cardinal;
+    i: Cardinal;
+begin
+    pos := HostRecordFind(host);
+    if pos <> -1 then
+    begin
+        WriteLn('HostRecordDel(): Found ', host, ' at pos ', pos);
+
+        ALength := Length(hostArray);
+        Assert(ALength > 0);
+        Assert(pos < ALength);
+        for i := pos + 1 to ALength - 1 do
+            hostArray[i - 1] := hostArray[i];
+        SetLength(hostArray, ALength - 1);
+    end;
+end; // of procedure HostRecordDel()
 
 
 procedure HostRecordShow();
 var
     i: integer;
 begin
-    writeln();
     writeln('HostRecordShow()');
-    
     for i := 0 to High(hostArray) do
     begin
-        writeln(i, ': ', hostArray[i].host);
+        
+        writeln('  ', i:4, ': ', hostArray[i].host);
     end; // of for
 end; // of procedure HostRecordShow
 
 
-procedure BuildTable(fn: Ansistring; sc: AnsiString; list: AnsiString);
+procedure BuildHostArray(fn: Ansistring; sc: AnsiString; list: AnsiString);
+//
+//  Build the arrayHost with existing host entries.
+//
 var
     tf: TextFile;
     line: integer;
@@ -108,6 +166,10 @@ var
 begin
     AssignFile(tf, fn);
     Reset(tf);
+
+    writeln('BuildHostArray()');
+    writeln(' Server Class : ', sc);
+    writeln(' List Type    : ', list);
 
     line := 0;
     inServerClass := false;
@@ -129,9 +191,12 @@ begin
         begin
             //writeln('-- FOUND LIST: ', list);
             inList := true;
-        end; // of if 
+        end // of if 
+        else 
+            inList := false;
 
         if Length(buffer) = 0 then
+            // When an empty line is encounterd then the server class has no more entries.
             inServerClass := false;
 
         writeln(line, ' inServerClass=', inServerClass, '   inList=', inList, ' > ', buffer);
@@ -153,31 +218,68 @@ begin
     end; // of while
 
     Close(tf);
-end; // of procedure BuildTable
+end; // of procedure BuildHostArray
+
+
+procedure WriteRecordToConfig(listType: AnsiString);
+var
+    i: Integer;
+begin
+    for i := 0 to High(hostArray) do
+    begin
+        writeln(listType, '.', i, ' = ', hostArray[i].host);
+    end; // of for
+end; // of procedure WriteRecordToConfig()
+
+
+procedure ProgUsage();
+begin
+    writeln();
+    writeln('scmod v0.3 - Splunk serverclass.conf modifier, add or delete hosts from a server class in whitelist and blacklist.');
+    writeln();
+    writeln('Usage: scmod <serverclass> <listtype> <action> <hostname>');
+    writeln('  <serverclass>        Name of the server class to modify');
+    writeln('  <listtype>           Select the "whitelist" or "blacklist"');
+    writeln('  <action>             What action to perform on the serverclass, select "add" or "delete"');
+    writeln('  <hostname>           Name of host to add or delete from the server class');
+    writeln();
+    Halt; // Stop the program.
+end; // of procedure ProgUsage()
 
 
 begin
-    fnOrg := './serverclass.conf';
-    //paramServerClass := 'svc_testclass';
-    paramServerClass := 'sc_emptyone';
-    paramList := 'whitelist';
-    paramAction := 'add';
-    paramHost := 'servertodo10';
+    if ParamCount <> 4 then
+        ProgUsage();
 
+    fnOrg := GetPathServerClass();
+    paramServerClass := paramStr(1);
+    paramList := paramStr(2);
+    paramAction := paramStr(3);
+    paramHost := paramStr(4);
 
+    WriteLn('Server Class Config file: ', fnOrg);
+    writeln('            Server Class: ', paramServerClass);
+    writeln('               List Type: ', paramList);
+    writeln('                  Action: ', paramAction);
+    writeln('                    Host: ', paramHost);
 
-    //BuildTable(fnOrg, paramServerClass, paramList);
+    // Build the aarayHost with the current entries of the server class
+    BuildHostArray(fnOrg, paramServerClass, paramList);
 
-    HostRecordAdd('totallynewserver');
-    HostRecordAdd('totallynewserver1');
-    HostRecordAdd('totallynewserver2');
-    HostRecordAdd('serverfind');
-    HostRecordAdd('totallynewserver4');
-    HostRecordAdd('totallynewserver');
-    HostRecordAdd('serverfind');
-    
-
+    // 
+    writeln('BEFORE ACTION');
     HostRecordShow();
+
+    if paramAction = 'add' then
+        HostRecordAdd(paramHost);
+
+    if paramAction = 'del' then
+        HostRecordDel(paramHost);
+
+    writeln('AFTER ACTION');
+    HostRecordShow();
+
+    WriteRecordToConfig(paramList);
 
     writeln('Program completed succesfully.');
 end. // of program ModifyServerClass

076b701be9d69c96d4b7302e87b7450773074818
Perry van den Hondel <perry.vandenhondel@gmail.com>
2019-06-28 09:30:50 +0200

    Check for server class in file.

--- scmod.pas
+++ scmod.pas
@@ -49,7 +49,41 @@ begin
 end; // of function GetPathServerClassConf
 
 
+procedure ProcessConfig(fn: AnsiString);
+var
+    tf: TextFile;
+    l: Integer;
+    buffer: AnsiString;
+    inServerClass: Boolean;
+begin
+    AssignFile(tf, fn);
+    Reset(tf);
 
+    l := 0;
+   
+    while not eof(tf) do
+    begin
+        ReadLn(tf, buffer);
+        Inc(l);
+        
+
+        if Pos('[serverClass:' + paramServerClass + ']', buffer) > 0 then
+        begin
+            //writeln('-- FOUND SERVERCLASS: ', sc);
+            inServerClass := true;
+        end; // of if 
+
+        if (inServerClass = true) and (Length(buffer) = 0) then
+        begin
+            // You are in the serverclass and encounter a empty line
+            // Stepped out of the serverclass
+            inServerClass := false;
+        end; // of if
+
+
+        WriteLn(l:4, ': INSERVERCLASS=', inServerClass, ' ', buffer);
+    end; // of while
+end; // of procedure ProcessConfig
 
 
 begin
@@ -61,6 +95,7 @@ begin
     paramAction := 'add';
     paramHost := 'lsrvnew01';
 
-
     WriteLn('Action on ', pathServerClassConf, ' in server class ', paramServerClass, ' for the ', paramListType,' to ', paramAction, ' ', paramHost);
+
+    ProcessConfig(pathServerClassConf);
 end. // of program ServerClassModifier

6a6f569a6c7b85286ccf9ac57c7958470027a2f4
Perry van den Hondel <perry.vandenhondel@gmail.com>
2019-11-15 13:31:48 +0100

    Housekeeping files

--- UBuild.pas
+++ UBuild.pas
@@ -5,8 +5,8 @@ interface
 const
 	VERSION_MAJOR = 2;
 	VERSION_MINOR = 0;
-	BUILD = 5;
-	BUILD_DT = '2019-09-30 08:56:35.065';
+	BUILD = 7;
+	BUILD_DT = '2019-11-15 13:18:32.006';
 
 implementation
 

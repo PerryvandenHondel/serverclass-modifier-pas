e7d3186c19f1c87c3e3adaf00934f5dfc2e469bf
Perry van den Hondel <perry.vandenhondel@gmail.com>
2019-12-24 16:53:45 +0100

    Build #327

--- ubuild.pas
+++ ubuild.pas
@@ -5,8 +5,8 @@ interface
 const
 	VERSION_MAJOR = 3;
 	VERSION_MINOR = 0;
-	BUILD = 318;
-	BUILD_DT = '2019-12-23 20:01:55.091';
+	BUILD = 327;
+	BUILD_DT = '2019-12-24 16:52:01.034';
 
 implementation
 
